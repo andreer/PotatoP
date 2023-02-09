ec508244b78d16749a3d020300f6571ecf56b97d
Andreas Eriksen <andreer@pvv.ntnu.no>
2023-02-09 23:49:54 +0100

    very basic input capture

--- lisp/type.lisp
+++ lisp/type.lisp
@@ -1,35 +1,34 @@
 ;; The humble beginnings of a text editor
 
-(defvar black 1)
-(defvar white 0)
+(defvar black 0)
+(defvar white 1)
 
 (defun insert-char (buffer char)
   (concatenate 'string buffer (princ-to-string char)))
 
 (defun display (buffer)
-  (fill-screen white)
+  (set-cursor 10 10)
   (set-text-color black white)
+  (fill-screen white)
   (with-gfx (out)
     (princ buffer out)
-    (princ #\176 out)))
+    (princ #\176 out))
+  (refresh))
 
-(defun type ()
-  (let ((buffer "")
-	(cursor-pos 1)
-	(font-width 6)
-	(font-height 8))
+(defun type () (edit ""))
+
+(defun edit (buffer)
+  (let ((cursor-pos 0))
+    (loop (if (not (get-key)) (return))) ; empty key buffer
     (fill-screen white)
-    (refresh)
+    (display buffer)
     (loop
      (let ((q (get-key)))
-       (if (and q (< q #xff))
-	   (progn
-	     (setq buffer (insert-char buffer (char-code q)))
-	     (incf cursor-pos)
-	     (display buffer)))))))
-
-
-	   ;(progn
-	   ;  (draw-char (* cursor-pos font-width) font-height (code-char q) black)
-	   ;  (set 'cursor-pos (+ cursor-pos 1))
-	   ;  (refresh)))))))
+       (cond
+	 ((not q) nothing)
+	 ((= q 27) (return buffer))
+	 ((= q 8)
+	  (setq buffer (subseq buffer 0 (1- (length buffer)))))
+         ((< q #xff)
+	  (setq buffer (insert-char buffer (code-char q)))))
+       (display buffer)))))
