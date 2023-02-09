bf1a62ae9c2c5d334b4e38359076b73f96813770
Andreas Eriksen <andreer@pvv.ntnu.no>
2023-02-09 23:49:54 +0100

    only redraw on changes

--- lisp/type.lisp
+++ lisp/type.lisp
@@ -7,7 +7,7 @@
   (concatenate 'string buffer (princ-to-string char)))
 
 (defun display (buffer)
-  (set-cursor 10 10)
+  (set-cursor 0 0)
   (set-text-color black white)
   (fill-screen white)
   (with-gfx (out)
@@ -16,20 +16,20 @@
   (refresh))
 
 (defun edit (buffer)
-  (let ((cursor-pos 0))
+  (let ((dirty t))
     (loop (if (not (get-key)) (return))) ; empty key buffer
     (fill-screen white)
-    (display buffer)
     (loop
      (let ((q (get-key)))
        (cond
-	 ((not q) nothing)
-	 ((= q 27) (return buffer))
-	 ((= q 8)
-	  (setq buffer (subseq buffer 0 (1- (length buffer)))))
-         ((< q #xff)
-	  (setq buffer (insert-char buffer (code-char q)))))
-       (display buffer)))))
+	 ((not q) (if dirty (progn (display buffer) (setq dirty nil))))
+	 ((= q (char-code #\Escape)) (return buffer))
+	 ((= q (char-code #\Backspace))
+	  (setq buffer (subseq buffer 0 (1- (length buffer)))
+		dirty t))
+	 ((< q #xff)
+	  (setq buffer (insert-char buffer (code-char q))
+		dirty t)))))))
 
 (defun type () (edit ""))
 
@@ -39,7 +39,12 @@
      (let ((typed (type)))
        (if (eq 0 (length typed)) (setq typed "nil"))
        (let ((evalled (eval (read-from-string typed))))
-	 (princ #\Newline gfx)
+	 (princ #\Newline gfx) (princ #\Newline gfx)
 	 (prin1 evalled gfx)
 	 (refresh)
 	 (loop (if (eq (get-key) 27) (return))))))))
+
+(defun repeat-string (str n)
+  (let ((res ""))
+    (dotimes (i n res)
+      (setq res (concatenate 'string res str)))))
