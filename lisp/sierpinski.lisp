ac1f0a8ef2792686411a4184d05362a5945d8937
Andreas Eriksen <andreer@pvv.ntnu.no>
2023-02-17 08:26:09 +0100

    actual triangles

--- lisp/sierpinski.lisp
+++ lisp/sierpinski.lisp
@@ -3,10 +3,14 @@
 (defun sierpinski (x0 y0 size gen)
   (when (plusp gen)
     (let ((s (ash size -1)) (n (1- gen)))
-      (fill-rect x0 y0 size size (col gen))
+      (fill-triangle
+       (+ x0 s) y0
+       x0 (+ y0 s s)
+       (+ x0 s s) (+ y0 s s)
+       (col gen))
       (refresh)
       (sierpinski (+ x0 (ash s -1)) y0 s n)
       (sierpinski x0 (+ y0 s) s n)
       (sierpinski (+ x0 s) (+ y0 s) s n))))
 
-(defun s-draw nil (set-rotation 0) (fill-screen 1) (sierpinski 40 0 240 7))
+(defun s-draw () (set-rotation 0) (fill-screen 1) (sierpinski 40 0 240 5))
