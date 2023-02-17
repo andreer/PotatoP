533025e8b984f7b8d9f3e3e369e7dcf9693508a7
Andreas Eriksen <andreer@pvv.ntnu.no>
2023-02-17 18:18:23 +0100

    iterative sum

--- lisp/stuff.lisp
+++ lisp/stuff.lisp
@@ -19,3 +19,12 @@
     (draw-char (random 320) (random 240) (code-char (1+ (random 2))) black white (1+ (random 10))))
   (refresh))
 
+(defun sum (list-of-numbers)
+  (let ((sum 0)
+	(head list-of-numbers))
+    (loop
+	  (if (null (car head)) (return)
+	      (setq sum (+ sum (car head))
+		    head (cdr head))))
+    sum))
+
